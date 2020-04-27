aea16d590e985a23b07b3d37e38ef2308f9b9e47
ng <64428454+ng121@users.noreply.github.com>
2020-04-28 01:03:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var prevdate = new Date("Apr 5,2019 14:05:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = now - prevdate;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+});
