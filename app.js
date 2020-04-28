716a088d6a8ec84c983e7d127f1049f06af24c6c
ng <64428454+ng121@users.noreply.github.com>
2020-04-28 11:45:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var prevdate = new Date("Apr 5,2019 14:05:00").getTime();
+var prevdate = new Date("Apr 5,2018 14:05:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = now - prevdate;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
+    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-        minutes + "m " + seconds + "s ";
+    document.getElementById("demo").innerHTML = years + " yr  " + days + " d  " + hours + " h  " +
+        minutes + " m  " + seconds + " s  ";
 });
